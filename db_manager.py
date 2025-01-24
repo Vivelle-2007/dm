7302a2f05a25a3f171e58527c9ffda4726c6f7a8
Vivelle-2007 <vivek931tiwari@gmail.com>
2025-01-24 16:49:45 +0530

    Update db_manager.py

--- db_manager.py
+++ db_manager.py
@@ -2,17 +2,17 @@ import pymongo
 import random
 import string
 
-# MongoDB connection
+
 MONGO_DB_URI = "mongodb+srv://vivek931tiwari:<db_password>@users.hdn7g.mongodb.net/?retryWrites=true&w=majority&appName=users"
 client = pymongo.MongoClient(MONGO_DB_URI)
 db = client['disaster_management_db']
 users_collection = db['users']
 
-# Function to generate a unique activation token
+
 def generate_token(length=6):
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
 
-# Function to register a user
+
 def register_user(phone, location):
     activation_token = generate_token()
     user_data = {
@@ -25,7 +25,7 @@ def register_user(phone, location):
     print(f"User registered successfully! Activation token: {activation_token}")
     return activation_token
 
-# Function to activate a user
+
 def activate_user(phone, token):
     user = users_collection.find_one({"phone": phone})
     if not user:
