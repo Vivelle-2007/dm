2dcac380ce1a0410dabddc2a279eebde69e0b73d
Vivelle-2007 <vivek931tiwari@gmail.com>
2025-01-24 16:48:54 +0530

    Update sms_sender.py

--- sms_sender.py
+++ sms_sender.py
@@ -1,6 +1,6 @@
 from twilio.rest import Client
 
-# Twilio configuration (replace with your own account SID, auth token, and phone number)
+
 TWILIO_ACCOUNT_SID = 'AC3ff1c0702e84ff30fced26400a2a2ef0'
 TWILIO_AUTH_TOKEN = '6a1b58f266465756750d7aa0345bfb8c'
 TWILIO_PHONE_NUMBER = '+1(682) 399-8721'
@@ -9,7 +9,6 @@ def send_sms(to_phone_number, message):
     """Send an SMS using Twilio."""
     client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
     try:
-        # Send the SMS message
         message = client.messages.create(
             body=message,
             from_=TWILIO_PHONE_NUMBER,
